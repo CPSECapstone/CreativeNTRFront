db315eee38d3ad59c99dda8af516917a2dac1cf1
GMOkeefe <35286980+GMOkeefe@users.noreply.github.com>
2020-11-12 07:00:31 -0800

    removed garbage from index

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,8 @@ import * as ReactDOM from 'react-dom';
 
 import { HelloComponent } from './hello';
 import { setup } from './setup';
-import { getComments } from './comments';
 
 setup();
-getComments();
 ReactDOM.render(
     <HelloComponent/>,
     document.getElementById('root')
