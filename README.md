6d397004d4a339e0f03c974c92c4d274f931863d
Nathan Irwin <nateirwin25@gmail.com>
2020-12-02 20:46:13 -0800

    Made toolbar accessible and refactored

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# CreativeNTRFront
+# CreativeNTR
+
+
+![test-actions](https://github.com/CPSECapstone/CreativeNTRFront/workflows/test-actions/badge.svg)
+
+This is the front end package for the annotation tool made for Cal Poly Creative Works. This is meant to be a plugin for their existing site written in react typescript.
+
+
+
+## Installation
+
+Use npm to install all dependencies
+
+```bash
+npm install
+```
+
+## Usage
+
+```python
+npm start
+```
+This will automatically host the page on port 3000 and open it in a browser
+
+
+CLI: https://github.com/CPSECapstone/CreativeNTRFront/actions
--- src/toolbar.tsx
+++ src/toolbar.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { startHighlight } from './highlight';
 
 
@@ -6,7 +6,7 @@ const toolbarStyle: React.HTMLAttributes<HTMLDivElement>["style"] = {
   display: 'flex',
   flexDirection: 'column',
   position: 'fixed',
-  top: '100px',
+  top: '25%',
   right: '0px',
   padding: '1px'
 };
@@ -32,29 +32,33 @@ const toolbarIconStyle: React.HTMLAttributes<HTMLDivElement>["style"] = {
   height: '45px'
 };
 
-class Toolbar extends Component {
+const toolbarButtonStyle: React.HTMLAttributes<HTMLDivElement>["style"] = {
+  borderStyle: "none"
+};
 
-  state = {
-    isActive: false,
+const Toolbar = () => {
+
+  const [isActive, changeIsActive] = React.useState(false);
+
+  const handleToggle = () => {
+    changeIsActive(!isActive);
   };
 
-  handleToggle = () => {
-    this.setState(
-      {
-        isActive: !this.state.isActive
-      });
-  };
-
-  render() {
-
+  if(isActive) {
     return (
       <div style={toolbarStyle} className="toolbar">
-        {!this.state.isActive && <button onClick={this.handleToggle}><img alt="edit" style={toolbarToggleStyle} src="edit.png" className="toolbarToggle" /></button>}
-        {this.state.isActive && <input type="image" alt="close" style={toolbarToggleStyle}  src="close.png" name="closeToolbar" className="toolbarToggle" onClick={this.handleToggle}/>}
-        {this.state.isActive && <div style={toolbarContentStyle} className="toolbarContent">
-          <input type="image" alt="highlight" style={toolbarIconStyle} src="highlight.png" name="highlighter" className="toolbarIcon" onClick={startHighlight}/>
-          <input type="image" alt="pen" style={toolbarIconStyle} src="pen.png" name="pen" className="toolbarIcon" />
-        </div>}
+        <button onClick={handleToggle} style={toolbarButtonStyle}><input type="image" alt="close" style={toolbarToggleStyle}  src="close.png" name="closeToolbar" className="toolbarToggle"/></button>
+        <div style={toolbarContentStyle} className="toolbarContent">
+          <button onClick={startHighlight} style={toolbarButtonStyle}><input type="image" alt="highlight" style={toolbarIconStyle} src="highlight.png" name="highlighter" className="toolbarIcon"/></button>
+          <button style={toolbarButtonStyle}><input type="image" alt="pen" style={toolbarIconStyle} src="pen.png" name="pen" className="toolbarIcon" /></button>
+        </div>
+      </div>
+    );
+  }
+  else {
+    return (
+      <div style={toolbarStyle} className="toolbar">
+        <button onClick={handleToggle} style={toolbarButtonStyle}><img alt="edit" style={toolbarToggleStyle} src="edit.png" className="toolbarToggle" /></button>
       </div>
     );
   }
